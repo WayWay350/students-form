331f2adcb57545c4e5a89eef9d241f21d8e95468
Igor Kasyanchuk <kicha930730@gmail.com>
2020-11-09 22:19:26 +0200

    comment

--- Views/index/hello.php
+++ Views/index/hello.php
@@ -5,28 +5,28 @@
 	<title></title>
 </head>
 <body>
-<table border="1px" style="height: 200px; margin: auto;">
- <th>ІМЯ</th>
- <th>ВІК</th>
- <th>СТАТЬ</th>
- <th>ГРУПА</th>
- <th>ФАКУЛЬТЕТ</th>
- <th>РЕДАГУВАТИ</th>
- <th>ВИДАЛИТИ</th>
+	<table border="1px" style="height: 200px; margin: auto;">
+ 		<th>ІМЯ</th>
+     	 <th>ВІК</th>
+ 		<th>СТАТЬ</th>
+ 		<th>ГРУПА</th>
+ 		<th>ФАКУЛЬТЕТ</th>
+ 		<th>РЕДАГУВАТИ</th>
+ 		<th>ВИДАЛИТИ</th>
 <?php
 #array with records from database
 foreach ($arg as $key => $value) {
 	?>
-	<tr>
-	<td style="text-align: center;"> <?php echo $value['name'] ?></td>
-	<td style="text-align: center;"> <?php echo $value['age'] ?></td>
-	<td style="text-align: center;"> <?php echo $value['sex'] ?></td>
-	<td style="text-align: center;"> <?php echo $value['grou'] ?></td>
-	<td style="text-align: center;"> <?php echo $value['faculty'] ?></td>
-	<td style="text-align: center;"> <a style="text-decoration: none; color: black; font-weight: bolder;" href="http://base/students/upgrade/<?php echo $value['id']?>">Press for edit</a></td>
-	<td style="text-align: center;"> <a style="text-decoration: none; color: black; font-weight: bolder;"href="http://base/students/delbase/<?php echo $value['id']?>">Press for delete</a></td>
-	</tr>
-	<?php
+		<tr>
+		<td style="text-align: center;"> <?php echo $value['name'] ?></td>
+		<td style="text-align: center;"> <?php echo $value['age'] ?></td>
+		<td style="text-align: center;"> <?php echo $value['sex'] ?></td>
+		<td style="text-align: center;"> <?php echo $value['grou'] ?></td>
+		<td style="text-align: center;"> <?php echo $value['faculty'] ?></td>
+		<td style="text-align: center;"> <a style="text-decoration: none; color: black; font-weight: bolder;" href="http://base/students/upgrade/<?php echo $value['id']?>">Press for edit</a></td>
+		<td style="text-align: center;"> <a style="text-decoration: none; color: black; font-weight: bolder;"href="http://base/students/delbase/<?php echo $value['id']?>">Press for delete</a></td>
+		</tr>
+<?php
 }
 ?>
 <!-- form for navigation buttons -->	
@@ -34,7 +34,7 @@ foreach ($arg as $key => $value) {
 	<tr><td colspan="7" style="text-align: center; background-color:#808080 "><input  style="font-size: 30px; background-color:#DCDCDC" type="submit" name="add" value="Add student"></td></tr>
 	<tr><td colspan="7" style="text-align: center; background-color:#808080 "><input style="font-size: 30px; background-color:#DCDCDC" type="submit" name="main" value="To main"></td></tr>
 	</form>
-</table>
+	</table>
 </body>
 </html>
 
